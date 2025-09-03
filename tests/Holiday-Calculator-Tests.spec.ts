1b3e6e07fc6dc4d89584c778e6c04666c365f311
Lewis Gibson <lewis.gibson@apdcomms.co.uk>
2025-09-03 17:38:59 +0100

    finished tests

--- tests/Holiday-Calculator-Tests.spec.ts
+++ tests/Holiday-Calculator-Tests.spec.ts
@@ -1,18 +1,35 @@
 import { test, expect } from '@playwright/test';
+import { HolidayCalculator } from './Helpers/Holiday-Calc-Helper';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.describe('Holiday Calculator Tests', () => {
+  
+  test('Full Year Worker 5 days a week', async ({ page }) => {
+    const calc = new HolidayCalculator(page);
+    await calc.gotostart();
+    await calc.chooseWorkerType('full-year');
+    await calc.ChoosenBasis('days');
+    await calc.ConfirmDates();
+    await calc.DaysWorked(5);
+    await calc.expectedResults('28 days Holiday');
+  });
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  test('Irregular hours, 1500 hours/year', async ({ page }) => {
+    const calc = new HolidayCalculator(page);
+    await calc.gotostart();
+    await calc.chooseWorkerType('irregular');
+    await calc.ConfirmDates();
+    await calc.EmploymentDates('2024-01-01', '2024-12-31');
+    await calc.HoursWorked(1500);
+    await calc.expectedResults('hours holiday');
+  });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  test('Part-year worker 3 days a week', async ({ page }) => {
+    const calc = new HolidayCalculator(page);
+    await calc.gotostart();
+    await calc.chooseWorkerType('part-year');
+    await calc.ChoosenBasis('days');
+    await calc.DaysWorked(3);
+    await calc.expectedResults('days Holiday');
+  });
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
